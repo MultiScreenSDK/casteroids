ead6d96bd248ba83e49a98a05fa387b2af75e7ab
Nik Bhattacharya <nibhatta@gmail.com>
2015-02-10 17:49:28 -0600

    Added ThreadUtils because we will have many occurences in which callbacks come in on a background thread and will need to call a method on the UI thread.

--- game-android/src/com/samsung/multiscreen/msf20/game/GameApplication.java
+++ game-android/src/com/samsung/multiscreen/msf20/game/GameApplication.java
@@ -3,6 +3,8 @@ package com.samsung.multiscreen.msf20.game;
 import android.app.Application;
 import android.util.Log;
 
+import com.samsung.multiscreen.msf20.game.utils.ThreadUtils;
+
 /**
  *
  * Game Application singleton.
@@ -17,6 +19,13 @@ public class GameApplication extends Application {
     public void onCreate() {
         super.onCreate();
         if(BuildConfig.DEBUG) Log.d(TAG, "Game Application created");
+
+        //store a reference to the UI thread
+        ThreadUtils.setUiThread(getMainLooper());
+
+        if(!ThreadUtils.runningOnUiThread()) {
+            throw new RuntimeException("The Application needs to be running on the UI Thread");
+        }
     }
 
 
