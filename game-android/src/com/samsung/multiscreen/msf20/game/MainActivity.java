cd27e124626e1d530a97791fe72b0c4be08b6033
Nik Bhattacharya <nibhatta@gmail.com>
2015-02-11 10:55:33 -0600

    Splash Screen, MainActivity and GameApplication added for the game-android client.

--- game-android/src/com/samsung/multiscreen/msf20/game/MainActivity.java
+++ game-android/src/com/samsung/multiscreen/msf20/game/MainActivity.java
@@ -2,8 +2,6 @@ package com.samsung.multiscreen.msf20.game;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
---
+++ game-android/src/com/samsung/multiscreen/msf20/game/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.samsung.multiscreen.msf20.game;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.samsung.multiscreen.msf20.game.utils.ThreadUtils;
+
+/**
+ * Splash Screen for the game.
+ *
+ * @author Nik Bhattacharya
+ *
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // make full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        // temporary
+        ThreadUtils.postOnUiThreadDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                launchActivity();
+            }
+        }, 1500);
+    }
+
+    private void launchActivity() {
+        boolean isConnected = true; // FIXME
+        if (isConnected) {
+            Intent mainActivityIntent = new Intent();
+            mainActivityIntent.setClass(this, MainActivity.class);
+            startActivity(mainActivityIntent);
+        } else {
+            Intent connectActivityIntent = new Intent();
+            connectActivityIntent.setClass(this, MainActivity.class); // FIXME
+            
+            startActivity(connectActivityIntent);
+        }
+    }
+
+}
