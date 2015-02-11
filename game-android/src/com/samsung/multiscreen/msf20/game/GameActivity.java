a18b4a32d49b7b0cfbc97ea30100b3a5f90df106
Nik Bhattacharya <nibhatta@gmail.com>
2015-02-11 16:09:16 -0600

    Added onTouchListeners to the buttons in the game view.

--- game-android/src/com/samsung/multiscreen/msf20/game/GameActivity.java
+++ game-android/src/com/samsung/multiscreen/msf20/game/GameActivity.java
@@ -2,11 +2,18 @@ package com.samsung.multiscreen.msf20.game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 
-public class GameActivity extends Activity {
+public class GameActivity extends Activity implements View.OnTouchListener {
+
+    private static final String TAG = GameActivity.class.getSimpleName();
+
+    private boolean turningLeft, turningRight, goingUp, goingDown, thrusting, firing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +26,102 @@ public class GameActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_game);
+        setOnTouchListners();
+    }
+
+    private void setOnTouchListners() {
+
+        int[] buttons = new int[]{R.id.up_button, R.id.down_button, R.id.left_button, R.id.right_button, R.id.thrust_button, R.id.fire_button};
+
+        for(int i = 0; i < buttons.length; i++) {
+            int buttonId = buttons[i];
+
+            View button = findViewById(buttonId);
+            button.setOnTouchListener(this);
+        }
     }
 
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
 
+        int id = v.getId();
+
+
+        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            handleDown(id);
+        } else if (event.getActionMasked() == MotionEvent.ACTION_UP) {
+            handleUp(id);
+        }
+
+
+        return false;
+    }
+
+
+    private void handleDown(int id) {
+
+        switch (id) {
+            case R.id.left_button:
+                turningLeft = true;
+                break;
+            case R.id.right_button:
+                turningRight = true;
+                break;
+            case R.id.up_button:
+                goingUp = true;
+                break;
+            case R.id.down_button:
+                goingDown = true;
+                break;
+            case R.id.thrust_button:
+                thrusting = true;
+                break;
+            case R.id.fire_button:
+                firing = true;
+                break;
+            default:
+                break;
+        }
+        Log.v(TAG, toString());
+    }
+
+    private void handleUp(int id) {
+
+        switch (id) {
+            case R.id.left_button:
+                turningLeft = false;
+                break;
+            case R.id.right_button:
+                turningRight = false;
+                break;
+            case R.id.up_button:
+                goingUp = false;
+                break;
+            case R.id.down_button:
+                goingDown = false;
+                break;
+            case R.id.thrust_button:
+                thrusting = false;
+                break;
+            case R.id.fire_button:
+                firing = false;
+                break;
+            default:
+                break;
+        }
+        Log.v(TAG, toString());
+    }
+
+    @Override
+    public String toString() {
+        return "GameActivity{" +
+                "turningLeft=" + turningLeft +
+                ", turningRight=" + turningRight +
+                ", goingUp=" + goingUp +
+                ", goingDown=" + goingDown +
+                ", thrusting=" + thrusting +
+                ", firing=" + firing +
+                '}';
+    }
 }
