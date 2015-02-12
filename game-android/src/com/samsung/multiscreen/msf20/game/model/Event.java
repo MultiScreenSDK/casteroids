5af73005bdd7dd622b398b5270203c7c6e53d111
Dan McCafferty <dan@hitwicketapps.com>
2015-02-11 18:46:10 -0600

    Adding flag to Event to indicate if it is sent or received by the client.

--- game-android/src/com/samsung/multiscreen/msf20/game/model/Event.java
+++ game-android/src/com/samsung/multiscreen/msf20/game/model/Event.java
@@ -7,27 +7,62 @@ package com.samsung.multiscreen.msf20.game.model;
  * 
  */
 public enum Event {
-	ROTATE("rotate"), THRUST("thrust"), FIRE("fire");
+    // Event sent to start/stop rotation of the spacecraft
+    ROTATE("rotate", true),
+    // Event sent to enable/disable the spacecraft's thrust.
+    THRUST("thrust", true),
+    // Event sent to have the spacecraft fire a bullet.
+    FIRE("fire", true),
+    // Event receive with the updated game score.
+    SCORE("score", false);
 
-	// The application defined name for the message event
-	private final String name;
+    // The application defined name for the message event
+    private final String name;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param name
-	 *            The application defined name for the message event
-	 */
-	Event(String name) {
-		this.name = name;
-	}
+    // A flag indicating whether or not the event is one the client sends versus
+    // one the client receives.
+    private final boolean send;
 
-	/**
-	 * Returns the application defined name for the message event.
-	 * 
-	 * @return
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * Constructor.
+     * 
+     * @param name
+     *            The application defined name for the message event
+     * @param send
+     *            A flag indicating whether or not the event is one the client
+     *            sends versus one the client receives.
+     */
+    Event(String name, boolean send) {
+        this.name = name;
+        this.send = send;
+    }
+
+    /**
+     * Returns the application defined name for the message event.
+     * 
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns a flag indicating whether or not the event is one the client
+     * sends this event.
+     * 
+     * @return
+     */
+    public boolean doesClientSend() {
+        return send;
+    }
+
+    /**
+     * Returns a flag indicating whether or not the event is one the client
+     * receives the event.
+     * 
+     * @return
+     */
+    public boolean doesClientReceive() {
+        return !send;
+    }
 }
