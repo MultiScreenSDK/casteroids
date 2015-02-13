ce254846d22936a13375c156eae56513d82d4000
Dan McCafferty <dan@hitwicketapps.com>
2015-02-13 10:50:34 -0600

    Adding placeholder for the dev url.

--- game-android/src/com/samsung/multiscreen/msf20/game/model/DevUrl.java
+++ game-android/src/com/samsung/multiscreen/msf20/game/model/DevUrl.java
@@ -1,9 +1,16 @@
 package com.samsung.multiscreen.msf20.game.model;
 
 /**
- * For Development purposes.
+ *
+ * This class will be checked in to the git repo and then added to the .gitignore.
+ * Developers can put the local url they are using for testing here.
+ *
+ *
  */
-
 public class DevUrl {
-
+    public static final String url = null; 
+    
+    public static boolean hasUrl() {
+        return (url != null);
+    }
 }
