91f87776247476437f87a43754bea9e7b3fae244
Nik Bhattacharya <nik@hitwicketapps.com>
2015-02-13 10:20:48 -0600

    DevUrl is for testing on local machines. Will add this to the gitignore once the initial version is pushed.

---
+++ game-android/src/com/samsung/multiscreen/msf20/game/model/DevUrl.java
@@ -0,0 +1,11 @@
+package com.samsung.multiscreen.msf20.game.model;
+
+/**
+ *
+ * This class will be checked in to the git repo and then added to the .gitignore.
+ * Developers can put the local url they are using for testing here.
+ *
+ *
+ */
+public class DevUrl {
+}
