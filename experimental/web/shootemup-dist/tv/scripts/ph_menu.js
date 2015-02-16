4456956242d69a37bbc99904329ddd2117f32c11
Adrian Hernandez <adrianhitwicket@gmail.com>
2015-02-16 17:41:26 -0600

    Rearranged folder structure moving my recent code out of the game-webapp into experimental

---
+++ experimental/web/shootemup-dist/tv/scripts/ph_menu.js
@@ -0,0 +1,44 @@
+
+BasicGame.MainMenu = function (game) {
+
+  this.music = null;
+  this.playButton = null;
+
+};
+
+BasicGame.MainMenu.prototype = {
+
+  create: function () {
+
+    //  We've already preloaded our assets, so let's kick right into the Main Menu itself.
+    //  Here all we're doing is playing some music and adding a picture and button
+    //  Naturally I expect you to do something significantly better :)
+
+    this.add.sprite(this.game.width / 2, 20, 'titlepage').anchor.setTo(0.5, 0.0);
+
+    this.loadingText = this.add.text(this.game.width / 2, this.game.height / 2 + 130, "Press Z or tap/click game to start", { font: "20px monospace", fill: "#fff" });
+    this.loadingText.anchor.setTo(0.5, 0.5);
+    this.add.text(this.game.width / 2, this.game.height - 75, "Copyright (c) 2015", { font: "12px monospace", fill: "#fff", align: "center"}).anchor.setTo(0.5, 0.5);
+
+  },
+
+  update: function () {
+
+    if (this.input.keyboard.isDown(Phaser.Keyboard.Z) || this.input.activePointer.isDown) {
+      this.startGame();
+    }
+    //  Do some nice funky main menu effect here
+
+  },
+
+  startGame: function (pointer) {
+
+    //  Ok, the Play Button has been clicked or touched, so let's stop the music (otherwise it'll carry on playing)
+    // this.music.stop();
+
+    //  And start the actual game
+    this.state.start('Game');
+
+  }
+
+};
