e1d141dea824e57e9b70a38744f21bc1b5908464
Adrian Hernandez <adrianhitwicket@gmail.com>
2015-02-16 13:27:07 -0600

    Added basic game strcuture and the initial implementation of a countdown timer

---
+++ game-webapp/dist/tv/scripts/ph_boot.js
@@ -0,0 +1,87 @@
+var BasicGame = {
+    // GAME CONSTANTS AND SETTINGS, THESE ARE FROM THE SHOOTEM UP SAMPLE; WE NEED TO DEFINE OUR OWN
+  SEA_SCROLL_SPEED: 12,
+  PLAYER_SPEED: 300,
+  ENEMY_MIN_Y_VELOCITY: 30,
+  ENEMY_MAX_Y_VELOCITY: 60,
+  SHOOTER_MIN_VELOCITY: 30,
+  SHOOTER_MAX_VELOCITY: 80,
+  BOSS_Y_VELOCITY: 15,
+  BOSS_X_VELOCITY: 200,
+  BULLET_VELOCITY: 500,
+  ENEMY_BULLET_VELOCITY: 150,
+  POWERUP_VELOCITY: 100,
+
+  SPAWN_ENEMY_DELAY: Phaser.Timer.SECOND,
+  SPAWN_SHOOTER_DELAY: Phaser.Timer.SECOND * 3,
+
+  SHOT_DELAY: Phaser.Timer.SECOND * 0.1,
+  SHOOTER_SHOT_DELAY: Phaser.Timer.SECOND * 2,
+  BOSS_SHOT_DELAY: Phaser.Timer.SECOND,
+
+  ENEMY_HEALTH: 2,
+  SHOOTER_HEALTH: 5,
+  BOSS_HEALTH: 500,
+
+  BULLET_DAMAGE: 1,
+  CRASH_DAMAGE: 5,
+
+  ENEMY_REWARD: 100,
+  SHOOTER_REWARD: 400,
+  BOSS_REWARD: 10000,
+  POWERUP_REWARD: 100,
+
+  ENEMY_DROP_RATE: 0.3,
+  SHOOTER_DROP_RATE: 0.5,
+  BOSS_DROP_RATE: 0,
+
+  PLAYER_EXTRA_LIVES: 3,
+  PLAYER_GHOST_TIME: Phaser.Timer.SECOND * 3,
+
+  INSTRUCTION_EXPIRE: Phaser.Timer.SECOND * 10,
+  RETURN_MESSAGE_DELAY: Phaser.Timer.SECOND * 2
+};
+
+BasicGame.Boot = function (game) {
+
+};
+
+BasicGame.Boot.prototype = {
+
+  init: function () {
+
+    //  Unless you specifically know your game needs to support multi-touch it's recommended setting this to 1
+    this.input.maxPointers = 1;
+
+    //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
+    // this.stage.disableVisibilityChange = true;
+
+    if (this.game.device.desktop) {
+      //  If you have any desktop specific settings, they can go in here
+    } else {
+      //  Same goes for mobile settings.
+      //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
+      this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.scale.setMinMax(480, 260, 1024, 768);
+      this.scale.forceLandscape = true;
+    }
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  },
+
+  preload: function () {
+
+    //  Here we load the assets required for our preloader (in this case a loading bar)
+    this.load.image('preloaderBar', 'assets/preloader-bar.png');
+
+  },
+
+  create: function () {
+
+    //  By this point the preloader assets have loaded to the cache, we've set the game settings
+    //  So now let's start the real preloader going
+    this.state.start('Preloader');
+
+  }
+
+};
---
+++ game-webapp/dist/tv/scripts/ph_game.js
@@ -0,0 +1,61 @@
+
+BasicGame.Game = function (game) {
+    this.secondsLeft = 180;
+    this.frame = 0;
+};
+
+BasicGame.Game.prototype = {
+
+  create: function () {
+      var style = { font: "14px Arial", fill: "#cccccc", align: "left" };
+      this.timerLabel = this.add.text(20, 20, this.timerLabel, style);
+  },
+
+  update: function () {
+    //  Main Game Loop
+      this.updateTimer();
+  },
+    
+    
+      updateTimer: function updateTimer() {
+      // This code displays the current system time minutes and seconds
+      /*minutes = Math.floor(this.game.time.time / 60000) % 60;
+      seconds = Math.floor(this.game.time.time / 1000) % 60;
+
+      //If any of the digits becomes a single digit number, pad it with a zero  
+      if (seconds < 10)
+         seconds = '0' + seconds;
+
+      if (minutes < 10)
+         minutes = '0' + minutes;*/
+
+          this.frame++;
+          if(this.frame == 60 ) {    // assuming 60 fps
+              this.frame = 0;
+              this.secondsLeft--;
+              
+              var minutes = Math.floor(this.secondsLeft/60);
+              var seconds = this.secondsLeft - minutes * 60;
+              if(seconds < 10) {
+                  seconds = '0'+seconds;
+              }
+              
+              this.timerLabel.setText(minutes+":"+seconds);
+              if(this.secondsLeft == 0) {
+                  this.quitGame();
+              }
+          }
+  },
+
+  quitGame: function (pointer) {
+
+    //  Here you should destroy anything you no longer need.
+    //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+
+    //  Then let's go back to the main menu.
+    this.state.start('GameOver');
+
+  }
+
+};
+
