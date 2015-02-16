5836646032d665a92879ab012c4bb3eee0d7b3e8
Adrian Hernandez <adrianhitwicket@gmail.com>
2015-02-16 15:55:08 -0600

    Added firing capabilities

--- game-webapp/dist/tv/scripts/ph_game.js
+++ game-webapp/dist/tv/scripts/ph_game.js
@@ -1,19 +1,47 @@
-
 BasicGame.Game = function (game) {
-    this.secondsLeft = 180;
+    this.secondsLeft = 120;
 };
 
 BasicGame.Game.prototype = {
 
   create: function () {
       var style = { font: "14px Arial", fill: "#cccccc", align: "left" };
-      this.timerLabel = this.add.text(20, 20, "03:00", style);
+      this.timerLabel = this.add.text(20, 20, "02:00", style);
+      this.instructionsLabel = this.add.text(this.game.width/2, this.game.height-20, "Press Z to Fire", style).anchor.setTo(0.5, 0.5);
       
+      this.cursors = this.input.keyboard.createCursorKeys();
       this.game.time.events.loop(1000, this.updateTimer, this);
+      
+      this.player = this.add.sprite(this.game.width / 2, this.game.height / 2, 'player');
+      this.player.anchor.setTo(0.5, 0.5);
+      this.player.animations.add('fly', [ 0, 1, 2 ], 20, true);
+      this.player.play('fly');
+      this.player.speed = 0;
+      this.player.direction = 0;
+      this.physics.enable(this.player, Phaser.Physics.ARCADE);
+      
+      this.bullets = [];
+      this.nextShotAt = 0;
+      this.shotDelay = 100;
   },
 
   update: function () {
       //  Main Game Loop
+      if (this.cursors.left.isDown) {
+          this.player.angle--;
+      } else if (this.cursors.right.isDown) {
+          this.player.angle++;
+      }
+      
+      if (this.cursors.up.isDown) {
+          this.player.speed++;
+      } else if (this.cursors.down.isDown) {
+          this.player.speed--;
+      }
+      if (this.input.keyboard.isDown(Phaser.Keyboard.Z) || this.input.activePointer.isDown) {
+          this.fire();
+      }
+      
   },
 
   updateTimer: function updateTimer() {
@@ -39,7 +67,33 @@ BasicGame.Game.prototype = {
     //  Then let's go back to the main menu.
     this.state.start('GameOver');
 
-  }
+  },
+    
+  fire: function() {
+      if (this.nextShotAt > this.time.now) {
+          return;
+      }
+      this.nextShotAt = this.time.now + this.shotDelay;
+      
+      var bullet = this.add.sprite(this.player.x, this.player.y - 20, 'bullet');
+      bullet.anchor.setTo(0.5, 0.5);
+      this.physics.enable(bullet, Phaser.Physics.ARCADE);
+      bullet.body.velocity.y = -500;
+      var color = 0;
+      switch (this.secondsLeft % 3) {
+              case 0:
+                color = 0xff0000;
+                break;
+              case 1:
+                color = 0x00ff00;
+                break;
+              case 2:
+                color = 0x0000ff;
+                break;
+      }
+      bullet.tint = color;
+      this.bullets.push(bullet);
+  },
 
 };
 
--- game-webapp/dist/tv/scripts/ph_preloader.js
+++ game-webapp/dist/tv/scripts/ph_preloader.js
@@ -25,6 +25,9 @@ BasicGame.Preloader.prototype = {
 
     //  Here we load the rest of the assets our game needs.
     this.load.image('titlepage', 'assets/phaser.png');
+    this.load.image('bullet', 'assets/bullet.png');
+    this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+    this.load.spritesheet('player', 'assets/player.png', 64, 64);
     /*this.load.image('sea', 'assets/sea.png');
     this.load.image('bullet', 'assets/bullet.png');
     this.load.image('enemyBullet', 'assets/enemy-bullet.png');
@@ -32,8 +35,6 @@ BasicGame.Preloader.prototype = {
     this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
     this.load.spritesheet('whiteEnemy', 'assets/shooting-enemy.png', 32, 32);
     this.load.spritesheet('boss', 'assets/boss.png', 93, 75);
-    this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
-    this.load.spritesheet('player', 'assets/player.png', 64, 64);
     this.load.audio('explosion', ['assets/explosion.ogg', 'assets/explosion.wav']);
     this.load.audio('playerExplosion', ['assets/player-explosion.ogg', 'assets/player-explosion.wav']);
     this.load.audio('enemyFire', ['assets/enemy-fire.ogg', 'assets/enemy-fire.wav']);
