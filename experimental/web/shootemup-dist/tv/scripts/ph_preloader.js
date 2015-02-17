4588a2dc581e158b2deb37726bb4f951f5527a8e
Adrian Hernandez <adrianhitwicket@gmail.com>
2015-02-16 18:07:27 -0600

    Added audio sprite to fire

--- experimental/web/shootemup-dist/tv/scripts/ph_preloader.js
+++ experimental/web/shootemup-dist/tv/scripts/ph_preloader.js
@@ -28,19 +28,7 @@ BasicGame.Preloader.prototype = {
     this.load.image('bullet', 'assets/bullet.png');
     this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
     this.load.spritesheet('player', 'assets/player.png', 64, 64);
-    /*this.load.image('sea', 'assets/sea.png');
-    this.load.image('bullet', 'assets/bullet.png');
-    this.load.image('enemyBullet', 'assets/enemy-bullet.png');
-    this.load.image('powerup1', 'assets/powerup1.png');
-    this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
-    this.load.spritesheet('whiteEnemy', 'assets/shooting-enemy.png', 32, 32);
-    this.load.spritesheet('boss', 'assets/boss.png', 93, 75);
-    this.load.audio('explosion', ['assets/explosion.ogg', 'assets/explosion.wav']);
-    this.load.audio('playerExplosion', ['assets/player-explosion.ogg', 'assets/player-explosion.wav']);
-    this.load.audio('enemyFire', ['assets/enemy-fire.ogg', 'assets/enemy-fire.wav']);
     this.load.audio('playerFire', ['assets/player-fire.ogg', 'assets/player-fire.wav']);
-    this.load.audio('powerUp', ['assets/powerup.ogg', 'assets/powerup.wav']);*/
-    //this.load.audio('titleMusic', ['audio/main_menu.mp3']);
     //  + lots of other required assets here
 
   },
