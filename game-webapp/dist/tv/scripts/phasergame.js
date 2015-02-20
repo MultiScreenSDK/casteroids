7515b31efb62cb8d69fb4d4b0987b0eda53914db
Dan McCafferty <dan@hitwicketapps.com>
2015-02-20 15:05:29 -0600

    git status

--- game-webapp/dist/tv/scripts/phasergame.js
+++ game-webapp/dist/tv/scripts/phasergame.js
@@ -1,7 +1,7 @@
 $(function(){
 
     "use strict"; 
-    var game = new Phaser.Game(1280, 800, Phaser.CANVAS, 'asteroids-example', {preload: preload, create: create, update: update, render: render});
+    var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'asteroids-example', {preload: preload, create: create, update: update, render: render});
 
     var sprite;
     var cursors;
@@ -17,7 +17,8 @@ $(function(){
     }
 
     function create() {
-
+        game.scale.parentIsWindow = true;
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         game.renderer.clearBeforeRender = false;
         game.renderer.roundPixels = true;
 
@@ -34,7 +35,7 @@ $(function(){
 
         //40 bullets
         bullets.createMultiple(40, 'bullet');
-        bullets.setAll('anchor.x', 0.5);
+        bullets.setAll('anchor.x', -1.0);
         bullets.setAll('anchor.y', 0.5);
 
         //ship
@@ -42,16 +43,10 @@ $(function(){
         sprite.anchor.set(0.5);
 
         //add the sprites physics
-        game.physics.enable(sprite, Phaser.Physics.ARCADE); //why this way and not settng the physicsBodyType??
+        game.physics.enable(sprite, Phaser.Physics.ARCADE); //why this way and not setting the physicsBodyType??
 
         sprite.body.drag.set(100);
         sprite.body.maxVelocity.set(200);
-
-        //Game input
-        cursors = game.input.keyboard.createCursorKeys();
-        game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
-
-
     }
 
     function update() {
@@ -63,18 +58,7 @@ $(function(){
             sprite.body.acceleration.set(0); //what is the difference between set(num) vs assignment via = sign?
         }
 
-        /*
-        if(turningLeft == true) {
-            //rotating left
-            sprite.body.angularVelocity = -300;
-        } else if(turningRight == true) {
-            //rotating right
-            sprite.body.angularVelocity = 300;
-        } else {
-            //no rotation
-            sprite.body.angularVelocity = 0;
-        }
-        */
+        // NOTE: The spacecraft's angular velocity is updated when handling rotate updates from the client.
 
         if(firing == true) {
             fireBullet();
@@ -126,57 +110,71 @@ $(function(){
             if(err) return console.error(err);
         });
 
-        channel.on('rotate', function(msg, from){
-            var rotateData = JSON.parse(msg);
-
-            var velocity = ((rotateData.strength * 400) / 20) + 100;
-
-            if(rotateData.rotate == 'left') {
-                //rotating left
-                sprite.body.angularVelocity = -velocity;
-            } else if(rotateData.rotate == 'right') {
-                //rotating right
-                sprite.body.angularVelocity = velocity;
-            } else {
-                //no rotation
-                sprite.body.angularVelocity = 0;
-            }
-
-            console.log((from.attributes.name || 'Unknown'));
-        });
-
-        channel.on('thrust', function(msg, from){
-            if(msg == 'on'){
-                thrusting = true;
-            } else {
-                thrusting = false;
-            }
-            console.log((from.attributes.name || 'Unknown'));
-        });
-
-        channel.on('fire', function(msg, from){
-            if(msg == 'on'){
-                firing = true;
-            } else {
-                firing = false;
-            }
-            console.log((from.attributes.name || 'Unknown'));
-        });
-
-        channel.on('clientConnect', function(client){
-            console.log('clientConnect');
-        });
-
-        channel.on('clientDisconnect', function(client){
-            console.log('clientDisconnect');
-        });
-
         channel.on('connect', function(client){
             console.log('connect');
         });
 
         channel.on('disconnect', function(client){
+            // TODO: Remove the client from the game
+            sprite.body.angularVelocity = 0;
+            thrusting = 0;
+            firing = 0;
             console.log('disconnect');
+        });
+
+        channel.on('clientConnect', function(client){
+            // TODO: Send the SlotData so the new client knows what slots are available.
+            console.log('clientConnect');
+        });
+
+        channel.on('clientDisconnect', function(client){
+            // TODO: Remove the client from the game
+            sprite.body.angularVelocity = 0;
+            thrusting = 0;
+            firing = 0;
+            console.log('clientDisconnect');
+        });
+
+        channel.on('join_request', function(msg, from) {
+            // TODO: Confirm the join and send back a join_response
+            console.log('join_request. from=' + (from.attributes.name || 'Unknown'));
+        });
+
+        channel.on('quit', function(msg, from) {
+            // TODO: Remove the client from the game (similar to clientDisconnect)
+            sprite.body.angularVelocity = 0;
+            thrusting = 0;
+            firing = 0;
+            console.log('fire. from=' + (from.attributes.name || 'Unknown'));
+        });
+
+        channel.on('rotate', function(msg, from){
+            // Parse the JSON data received from the client.
+            var rotateData = JSON.parse(msg);
+
+            // Map the 0 to 20 range value from the message's data to a 100 to 400 range value for the game.
+            var velocity = ((rotateData.strength * 400) / 20) + 100;
+
+            // Update the angular velocity based on the rotate direction (right, left, or none).
+            if(rotateData.rotate == 'left') {
+                sprite.body.angularVelocity = -velocity;
+            } else if(rotateData.rotate == 'right') {
+                sprite.body.angularVelocity = velocity;
+            } else {
+                sprite.body.angularVelocity = 0;
+            }
+
+            console.log('rotate. from=' + (from.attributes.name || 'Unknown'));
+        });
+
+        channel.on('thrust', function(msg, from){
+            thrusting = (msg == 'on');
+            console.log('thrust. from=' + (from.attributes.name || 'Unknown'));
+        });
+
+        channel.on('fire', function(msg, from){
+            firing = (msg == 'on');
+            console.log('fire. from=' + (from.attributes.name || 'Unknown'));
         });
 
     });
