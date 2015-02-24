4d5b3e7b1e2710ee03d0d21eb010caccc42b261b
Dan McCafferty <dan@hitwicketapps.com>
2015-02-23 18:20:52 -0600

    Preparing for hooking up for multiple clients. All the methods now get the object with the player object placeholder.

--- game-webapp/dist/tv/scripts/phasergame.js
+++ game-webapp/dist/tv/scripts/phasergame.js
@@ -8,12 +8,14 @@ $(function(){
     var bullet;
     var bullets;
     var bulletTime = 0;
-    var thrusting, firing;
+    var thrusting;
 
+    //  A placeholder for one player in the gave
     function Slot(color, colorCode) {
         this.color = color || 'unknown';
         this.colorCode = colorCode || 0x000000;
         this.available = true;
+        // TODO: Move the clientId to the Player object. It's here because there currently isn't a Player object.
         this.clientId = null;
         this.player = null;
     }
@@ -29,102 +31,17 @@ $(function(){
         ERROR: 2
     };
 
+    // Store client id to slot mappings.
     var clientIdToSlotMap = {};
 
+    // Stores color to slot mappings.
     var colorToSlotMap = {};
+
+    // Initialize the color to slot mappings.
     for (var i in slots) {
         var slot = slots[i];
         colorToSlotMap[slot.color] = slot;
         console.log('adding '+slot);
-    }
-
-    function addPlayer(clientId, name, color) {
-        // If we already have a player with this client id, remove it and add the new one.
-        removePlayer(clientId);
-
-        // Look up the requested slot by the color name
-        var slot = colorToSlotMap[color];
-
-        // If the slot is not valid, return the ERROR code.
-        if (slot == null) {
-            return JoinResponseCode.ERROR;
-        }
-        // Else if the slot is not available, return the COLOR_TAKEN code.
-        else if (!slot.available)  {
-            return JoinResponseCode.COLOR_TAKEN;
-        }
-
-        // Associate the client to the slot
-        slot.available = false;
-        slot.clientId = clientId;
-
-        // Create add add the Player object to the game and slot.
-        // TODO: Create and add the player to the game and slot
-        // slot.player = player;
-
-        // Add the client id to slot mapping
-        clientIdToSlotMap[clientId] = slot;
-
-        // Return the SUCCESS code
-        return JoinResponseCode.SUCCESS;
-    }
-
-    function removePlayer(clientId) {
-        // Look up the requested slot by the color name
-        var slot = clientIdToSlotMap[clientId];
-
-        // If the slot is null then the player has already been removed or the given client id is invalid.
-        if (slot == null) {
-            return;
-        }
-
-        // Unassociate the client with the slot
-        slot.available = true;
-        slot.clientId = null;
-
-        // Remove the player from the game and slot
-        // TODO: Remove the player from the game
-        slot.player = null;
-
-        // TODO: Remove this section after the above TODO is complete.
-        // For now we are just stopping the ship from moving around after a disconnect
-        sprite.body.angularVelocity = 0;
-        thrusting = 0;
-        firing = 0;
-
-        // Remove client id to slot mapping
-        delete clientIdToSlotMap[clientId];
-    }
-
-    function onRotatePlayer(clientId, direction, strength) {
-        // Look up the requested slot by the color name
-        var slot = clientIdToSlotMap[clientId];
-
-        // If the slot is null then the player has been removed or the given client id is invalid.
-        if (slot == null) {
-            return;
-        }
-
-        // TODO: Make modifications on the player object on the slot object
-        // Map the 0 to 20 range value from the message's data to a 100 to 400 range value for the game.
-        var velocity = ((strength * 400) / 20) + 100;
-
-        // Update the angular velocity based on the rotate direction (right, left, or none).
-        if(direction == 'left') {
-            sprite.body.angularVelocity = -velocity;
-        } else if(direction == 'right') {
-            sprite.body.angularVelocity = velocity;
-        } else {
-            sprite.body.angularVelocity = 0;
-        }
-    }
-
-    function onThrust(clientId, thrustEnabled) {
-        thrusting = thrustEnabled;
-    }
-
-    function onFire(clientId, gunsEnabled) {
-        firing = gunsEnabled;
     }
 
     function preload() {
@@ -175,18 +92,125 @@ $(function(){
             sprite.body.acceleration.set(0); //what is the difference between set(num) vs assignment via = sign?
         }
 
-        // NOTE: The spacecraft's angular velocity is updated when handling rotate updates from the client.
-
-        if(firing == true) {
-            fireBullet();
-        }
+        // NOTE: The spacecraft's angular velocity is updated when handling rotate events from the client.
+        // NOTE: The spacecraft's firing of bullets is performed when handling fire events from the client.
 
         screenWrap(sprite);
 
         bullets.forEachExists(screenWrap, this); //what is this??
     }
 
-    function fireBullet() {
+    // Attempts to add a player to the game. Called when a client requests to join the game.
+    function addPlayer(clientId, name, color) {
+        // If we already have a player with this client id, remove it and add the new one.
+        removePlayer(clientId);
+
+        // Look up the requested slot by the color name
+        var slot = colorToSlotMap[color];
+
+        // If the slot is not valid, return the ERROR code.
+        if (slot == null) {
+            return JoinResponseCode.ERROR;
+        }
+        // Else if the slot is not available, return the COLOR_TAKEN code.
+        else if (!slot.available)  {
+            return JoinResponseCode.COLOR_TAKEN;
+        }
+
+        // Associate the client to the slot
+        slot.available = false;
+        slot.clientId = clientId;
+
+        // Create add add the Player object to the game and slot.
+        // TODO: Create and add the player to the game and slot
+        // slot.player = player;
+
+        // Add the client id to slot mapping
+        clientIdToSlotMap[clientId] = slot;
+
+        // If the game was not already initiated then start the countdown!
+        // TODO: If the game was not already initiated then start the countdown!
+
+        // Return the SUCCESS code
+        return JoinResponseCode.SUCCESS;
+    }
+
+    // Attempts to remove a player from the game. Called when a player quits or a client disconnects.
+    function removePlayer(clientId) {
+        // Look up the requested slot by the color name
+        var slot = clientIdToSlotMap[clientId];
+
+        // If the slot is null then the player has already been removed or the given client id is invalid.
+        if (slot == null) {
+            return;
+        }
+
+        // Un-associate the client with the slot
+        slot.available = true;
+        slot.clientId = null;
+
+        // Remove the player from the game and slot
+        // TODO: Remove the player from the game
+        slot.player = null;
+
+        // TODO: Remove this section after the above TODO is complete.
+        // For now we are just stopping the ship from moving around after a disconnect
+        sprite.body.angularVelocity = 0;
+        thrusting = 0;
+
+        // Remove client id to slot mapping
+        delete clientIdToSlotMap[clientId];
+    }
+
+    // Rotate the client's spaceship. Called when a client sends a rotate command.
+    function onRotatePlayer(clientId, direction, strength) {
+        // Look up the requested slot by the color name
+        var slot = clientIdToSlotMap[clientId];
+
+        // If the slot is null then the player has been removed or the given client id is invalid.
+        if (slot == null) {
+            return;
+        }
+
+        // TODO: Make modifications on the player object on the slot object
+        // Map the 0 to 20 range value from the message's data to a 100 to 400 range value for the game.
+        var velocity = ((strength * 400) / 20) + 100;
+
+        // Update the angular velocity based on the rotate direction (right, left, or none).
+        if(direction == 'left') {
+            sprite.body.angularVelocity = -velocity;
+        } else if(direction == 'right') {
+            sprite.body.angularVelocity = velocity;
+        } else {
+            sprite.body.angularVelocity = 0;
+        }
+    }
+
+    // Enables thrust on a clients spaceship. Called when the client sends a thrust event.
+    function onThrust(clientId, thrustEnabled) {
+        // Look up the requested slot by the color name
+        var slot = clientIdToSlotMap[clientId];
+
+        // If the slot is null then the player has been removed or the given client id is invalid.
+        if (slot == null) {
+            return;
+        }
+
+        // TODO: Make modifications on the player object on the slot object
+        thrusting = thrustEnabled;
+    }
+
+    // Fires a bullet from the client's spaceship. Called when the client sends a fire event.
+    function onFire(clientId) {
+        // Look up the requested slot by the color name
+        var slot = clientIdToSlotMap[clientId];
+
+        // If the slot is null then the player has been removed or the given client id is invalid.
+        if (slot == null) {
+            return;
+        }
+
+        // TODO: Make modifications on the player object on the slot object
         if(game.time.now > bulletTime) {
             bullet = bullets.getFirstExists(false); //what is this?
 
@@ -274,6 +298,9 @@ $(function(){
             // If the client successfully joined, send out the slot data update.
             if (responseCode == JoinResponseCode.SUCCESS) {
                 sendSlotUpdate('all');
+
+                // TODO: REMOVE statement below after the addPlayer method's TODO for starting the game is complete.
+                sendGameStart(0);
             }
         });
 
@@ -300,7 +327,9 @@ $(function(){
 
         channel.on('fire', function(msg, from){
             console.log('fire. from=' + (from.id || 'Unknown'));
-            onFire(from.id, msg == 'on');
+            if (msg == 'on') {
+                onFire(from.id);
+            }
         });
 
         function sendSlotUpdate(to) {
@@ -311,9 +340,27 @@ $(function(){
                 slotData[i] = { available : slot.available, color : slot.color};
             }
 
-            // Send a slot_update to the client
+            // Send a slot_update to the client(s)
             console.log('sending slot_update ' + JSON.stringify(slotData) + ". to=" + to);
             channel.publish('slot_update', JSON.stringify(slotData), to);
+        }
+
+        function sendGameStart(countdown) {
+            // Send a game_start to all clients
+            console.log('sending game_start ' + countdown + " secs. to=all");
+            channel.publish('game_start', countdown);
+        }
+
+        function sendPlayerOut(clientId, countdown) {
+            // Send a player_out to the client
+            console.log('sending player_out ' + countdown + " secs. to=" + clientId);
+            channel.publish('player_out', countdown, clientId);
+        }
+
+        function sendGameOver() {
+            // Send a game_over to all clients
+            console.log('sending game_over. to=all');
+            channel.publish('game_over', null);
         }
 
     });
