f14859b65998eed995711359ab7b5d210d3d8446
Adrian Hernandez <adrianhitwicket@gmail.com>
2015-02-26 17:12:22 -0600

    Refactored some Global constants, added some of the scoring mechanics from the prd and fixed indentation throughout all the files (BEWARE CONFLICTS)

--- game-webapp/dist/tv/scripts/ph_boot.js
+++ game-webapp/dist/tv/scripts/ph_boot.js
@@ -1,30 +1,33 @@
 var BasicGame = {
     GAME_LENGTH: 120,
     GAME_COUNTDOWN_LENGTH: 5,
+    GAME_OVER_DELAY: 5000,
     ORIENTATION_CORRECTION: Math.PI/2,
     HIT_POW: 10,
-    HIT_SCORE: 10,
+    ALIEN_HIT_SCORE: 10,
+    ALIEN_DESTROY_SCORE: 50,
+    PLAYER_DESTROY_SCORE: 100,
     HIT_DEDUCT: 10,
-    PLAYER_HP: 50,
+    PLAYER_HP: 10,
     PLAYER_COLOR: 0x00ff00,
     PLAYER_DRAG: 100,
     PLAYER_MAX_SPEED: 180,
     PLAYER_BULLET_SPEED: 400,
     PLAYER_FIRE_DELAY: 300,
     PLAYER_FIRE_RANGE: 1000,
-    PLAYER_RESPAWN_DELAY: 3000,
+    PLAYER_RESPAWN_DELAY: 2000,
     PLAYER_ACC_SPEED: 200,
     PLAYER_TURNING_SPEED: 200,
-    ENEMY_HP: 30,
-    ENEMY_COLOR: 0xff0000,
-    ENEMY_DRAG: 100,
-    ENEMY_MAX_SPEED: 70,
-    ENEMY_BULLET_SPEED: 300,
-    ENEMY_FIRE_DELAY: 1000,
-    ENEMY_FIRE_RANGE: 600,
-    ENEMY_HOMING_SPEED: 50,
-    ENEMY_HOMING_MAX_TIME: 200,
-    ENEMY_RESPAWN_DELAY: 2000,
+    ALIEN_HP: 100,
+    ALIEN_COLOR: 0xff0000,
+    ALIEN_DRAG: 100,
+    ALIEN_MAX_SPEED: 70,
+    ALIEN_BULLET_SPEED: 300,
+    ALIEN_FIRE_DELAY: 1000,
+    ALIEN_FIRE_RANGE: 600,
+    ALIEN_HOMING_SPEED: 50,
+    ALIEN_HOMING_MAX_TIME: 200,
+    ALIEN_RESPAWN_DELAY: 2000,
 };
 
 BasicGame.Boot = function (game) {
@@ -33,23 +36,23 @@ BasicGame.Boot = function (game) {
 
 BasicGame.Boot.prototype = {
 
-  init: function () {
-    //  We re not using multi-touch so it's recommended setting this to 1
-    this.input.maxPointers = 1;
-    this.scale.pageAlignHorizontally = true;
-    this.scale.pageAlignVertically = true;
-  },
+    init: function () {
+        //  We re not using multi-touch so it's recommended setting this to 1
+        this.input.maxPointers = 1;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+    },
 
-  preload: function () {
-    //  Here we load the assets required for our preloader (in this case a loading bar)
-    this.load.image('preloaderBar', 'assets/preloader-bar.png');
-  },
+    preload: function () {
+        //  Here we load the assets required for our preloader (in this case a loading bar)
+        this.load.image('preloaderBar', 'assets/preloader-bar.png');
+    },
 
-  create: function () {
-    //  By this point the preloader assets have loaded to the cache, we've set the game settings
-    //  So now let's start the real preloader going
-    this.state.start('Preloader');
+    create: function () {
+        //  By this point the preloader assets have loaded to the cache, we've set the game settings
+        //  So now let's start the real preloader going
+        this.state.start('Preloader');
 
-  }
+    }
 
 };
