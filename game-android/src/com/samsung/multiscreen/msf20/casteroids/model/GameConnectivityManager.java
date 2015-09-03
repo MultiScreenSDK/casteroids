3ea02e118a77b865fc5f72bc951c582ae153050e
Bin Liu <bliu@samsung.com>
2015-09-02 18:14:14 -0700

    Update the code to use product tv url instead od dev-multiscreen.com

--- game-android/src/com/samsung/multiscreen/msf20/casteroids/model/GameConnectivityManager.java
+++ game-android/src/com/samsung/multiscreen/msf20/casteroids/model/GameConnectivityManager.java
@@ -28,7 +28,8 @@ public class GameConnectivityManager extends ConnectivityManager implements Conn
 	protected static final String PROD_TV_APP_URL = "http://multiscreen.samsung.com/casteroids/tv/index.html";
 
 	// The URL for the TV application
-	private static final String TV_APP_URL = DEV_TV_APP_URL;
+	//private static final String TV_APP_URL = DEV_TV_APP_URL;
+	private static final String TV_APP_URL = PROD_TV_APP_URL;
     
 	// The Channel ID for the TV application
 	private static final String TV_APP_CHANNEL_ID = "com.samsung.multiscreen.casteroids";
